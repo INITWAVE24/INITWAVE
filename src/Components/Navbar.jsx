196f5423a5d66865e12483859ab3d08d5a9f2b4c
yashaswini0001 <yashuyashi333@gmail.com>
2024-05-06 22:42:27 +0530

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo1 from "../Images/logo1.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes, faArrowRight } from '@fortawesome/free-solid-svg-icons'; // Added faTimes icon
 import { motion } from 'framer-motion'
 import "../App.css";
 import { NavLink } from 'react-router-dom';
@@ -39,7 +39,8 @@ function Navbar() {
                     </div>
                 </div>
                 <div className="mobile-menu-toggle" onClick={toggleMobileMenu}>
-                    <FontAwesomeIcon icon={faBars} />
+                   
+                    <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
                 </div>
             </div>
             <div className={`mobile-menu-contents ${isMobileMenuOpen ? 'open' : ''}`}>
@@ -54,22 +55,7 @@ function Navbar() {
                         <li>SERVICES</li>
                     </NavLink>
                     <NavLink to='/careers' activeClassName="active" smooth={true} offset={0} duration={500}>
-                        <li
-                            onMouseEnter={toggleDropdown}
-                            onMouseLeave={toggleDropdown}
-                            className="dropdown-toggle"
-                        >
-                            CAREERS
-                            <div className={`dropdown-content ${isDropdownOpen ? 'open' : ''}`}>
-                                <p>Join us at Interact IT and become a part of a community that values innovation, collaboration, and making a positive difference in the world. Together, we can create meaningful solutions that shape the future of technology and drive positive change for our clients and communities alike.</p>
-                                <NavLink to='/careers'>
-                                    <motion.button whileTap={{ scale: 0.85 }}>
-                                        Explore Opportunities
-                                        <FontAwesomeIcon icon={faArrowRight} className='arrow-icon' />
-                                    </motion.button>
-                                </NavLink>
-                            </div>
-                        </li>
+                        <li>CAREERS</li>
                     </NavLink>
                     <NavLink to='/contact' activeClassName="active" smooth={true} offset={0} duration={500}>
                         <li>CONTACT</li>
